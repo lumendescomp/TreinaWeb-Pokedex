dfcacb9964c14d1e6342611f4b0d62eb88b75ab8
Lipe Mendes <82170079+lumendescomp@users.noreply.github.com>
2021-10-25 17:40:48 -0300

    Add files via upload

---
+++ TreinaWeb JS/JS TreinaWeb/scripts/list-service.js
@@ -0,0 +1,11 @@
+//Renderizando nossa lista de pokemons no hml
+var ListService = {
+	createList: function (pkmList) {
+		return pkmList.map(function (pokemon) {
+			return '<li class="poke-list-item" >' +
+				'<img src="//serebii.net/pokedex-xy/icon/' + pokemon.number + '.png" />' +
+				'<span>' + pokemon.number + ' - ' + pokemon.name + '</span>' +
+				'</li>';
+		}).join('')
+	}
+}
